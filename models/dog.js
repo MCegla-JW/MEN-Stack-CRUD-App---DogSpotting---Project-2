e8fd9281c985234942716300a9ec95d403812196
MCegla-JW <m.cegla93@gmail.com>
2025-11-02 15:27:54 +0000

    defined dog and user Schemas and created models

---
+++ models/dog.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const dogSchema = new mongoose.Schema ({
+    name: {type: String, required: true}, 
+    breed: {type: String, required: true}, 
+    age: {type: Number, required: true},
+    location: {type: String, required: true},
+    description: {type: String, required: true}, 
+    photoURL: {ype: String},
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, 
+    likedByUsers: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+}) 
+
+const Dog = mongoose.model('Dog', dogSchema)
