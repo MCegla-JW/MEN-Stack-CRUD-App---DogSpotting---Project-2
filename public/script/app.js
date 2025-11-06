32e6e5ce760f3ca6e255f3c8447d276eb64e5a34
MCegla-JW <m.cegla93@gmail.com>
2025-11-06 18:21:57 +0000

    created a hamburger menu for mobile and fixed sign-in and sign-up button issues on homepage

--- public/script/app.js
+++ public/script/app.js
@@ -2,10 +2,21 @@ const openDeleteModalBtn = document.querySelector('#open-delete-modal')
 const deleteModalEl = document.querySelector('#delete-modal')
 const cancelDeleteBtn = document.querySelector('#cancel-delete')
 
+if (openDeleteModalBtn && deleteModalEl && cancelDeleteBtn) {
 const toggleDeleteModal = (e) => {
     deleteModalEl.classList.toggle('show')
-
 }
 
 openDeleteModalBtn.addEventListener('click', toggleDeleteModal)
 cancelDeleteBtn.addEventListener('click', toggleDeleteModal)
+}
+
+const hamMenu = document.querySelector('.ham-menu')
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+if (hamMenu && offScreenMenu) {
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
+}
